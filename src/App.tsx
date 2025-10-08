f4afd14f67ab22f75b7ba1323c8ec7771aa42e7b
ThiagoLopesSantos <thiago8343@gmail.com>
2025-10-08 15:49:33 -0300

    Configurando o Redux e criando os reducers do carrinho

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyle } from './styles'
 import Footer from './Container/Footer'
 import Ways from './routes'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Ways />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Ways />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
